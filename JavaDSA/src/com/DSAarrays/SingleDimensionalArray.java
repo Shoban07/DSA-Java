cf0b808d42edab1943c12b697dcc085057e547f6
Shoban07 <shobankumar.r96@gmail.com>
2021-10-06 02:58:08 +0530

    One D and 2 D arrays

--- JavaDSA/src/com/DSAarrays/SingleDimensionalArray.java
+++ JavaDSA/src/com/DSAarrays/SingleDimensionalArray.java
@@ -3,7 +3,7 @@ package com.DSAarrays;
 import java.util.Arrays;
 import java.util.Scanner;
 
-public class TakeInputForArray
+public class SingleDimensionalArray
 {
     public static void main(String[] args) {
         System.out.println();
@@ -13,18 +13,35 @@ public class TakeInputForArray
         int arraySize = scn.nextInt();
         scn = new Scanner(System.in);
         int[] arr = new int[arraySize];
-        System.out.printf("Entered size of the array: "+arraySize);
+        System.out.print("Entered size of the array: "+arraySize);
+
         //take input for size of the array
         for (int i = 0; i < arraySize; i++)
         {
                 arr[i] = scn.nextInt();
         }
         //print the input
-        for(int num : arr)
+        /*
+        * Way 1:  using for loop*/
+        System.out.println("using for loop");
+        for(int i = 0; i<arraySize;i++)
         {
-            System.out.printf(num+", ");
+            System.out.println(arr[i]+", ");
         }
         System.out.println();
+        System.out.println("using for each loop");
+        /*
+        * Way 2: Using Enhanced for loop, that is the 'For Each loop'*/
+        for(int num : arr)
+        {
+            System.out.print(num+", ");
+        }
+        System.out.println();
+
+        /*
+        * using to string method
+        * */
+        System.out.println("using Arrays.toString method");
         System.out.println(Arrays.toString(arr));
     }
 }
