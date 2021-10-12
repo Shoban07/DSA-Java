6f374c0b70bbb1e2311cd378b9949cf42aa74370
Shoban07 <shobankumar.r96@gmail.com>
2021-10-13 00:12:06 +0530

    day 2 Assignment Completion in arrays

--- JavaDSA/src/com/DSAarrays/Assignments/NumberOfGoodPairs.java
+++ JavaDSA/src/com/DSAarrays/Assignments/NumberOfGoodPairs.java
@@ -3,24 +3,41 @@ package com.DSAarrays.Assignments;
 //Difficulty : Easy 7. Number of Good Pairs
 //reference link : https://leetcode.com/problems/number-of-good-pairs/
 /*
-* There are n kids with candies. You are given an integer array candies, where each candies[i] represents the number of candies the ith kid has, and an integer extraCandies, denoting the number of extra candies that you have.
+* Given an array of integers nums, return the number of good pairs.
 
-Return a boolean array result of length n, where result[i] is true if, after giving the ith kid all the extraCandies, they will have the greatest number of candies among all the kids, or false otherwise.
-
-Note that multiple kids can have the greatest number of candies.
-
-
+A pair (i, j) is called good if nums[i] == nums[j] and i < j.
 
 Example 1:
 
-Input: candies = [2,3,5,1,3], extraCandies = 3
-Output: [true,true,true,false,true]
-Explanation: If you give all extraCandies to:
-- Kid 1, they will have 2 + 3 = 5 candies, which is the greatest among the kids.
-- Kid 2, they will have 3 + 3 = 6 candies, which is the greatest among the kids.
-- Kid 3, they will have 5 + 3 = 8 candies, which is the greatest among the kids.
-- Kid 4, they will have 1 + 3 = 4 candies, which is not the greatest among the kids.
-- Kid 5, they will have 3 + 3 = 6 candies, which is the greatest among the kids.*/
+Input: nums = [1,2,3,1,1,3]
+Output: 4
+Explanation: There are 4 good pairs (0,3), (0,4), (3,4), (2,5) 0-indexed.*/
 
 public class NumberOfGoodPairs {
+    public int NumIdenticalPairs(int[] nums) {
+        /*int count = 0;
+        for(int i = 0;i<nums.length;i++)
+        {
+            for(int j=i+1;j<nums.length;j++)
+            {
+                if(nums[i]==nums[j])
+                {
+                    count++;
+                }
+            }
+        }
+        return count;*/
+        int res = 0, count[] = new int[101];
+        for(int n: nums){
+            res = res + count[n];
+            count[n]++;
+        }
+        return res;
+    }
+
+    public static void main(String[] args) {
+        int[] arr = {1, 2, 3, 1, 1, 3};
+        NumberOfGoodPairs gp = new NumberOfGoodPairs();
+        System.out.println(gp.NumIdenticalPairs(arr));
+    }
 }
